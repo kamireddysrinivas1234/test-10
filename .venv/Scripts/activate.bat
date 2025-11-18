2927a454a0228d6b4f24534faeac64fdb4fd3c8c
kamireddysrinivas1234 <kamireddysrinivas1234@gmail.com>
2025-11-18 11:38:01 -0500

    test

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asus\Desktop\web 10 ass 23\.venv
+set VIRTUAL_ENV=C:\Users\Asus\Desktop\web 10 ass 24\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
